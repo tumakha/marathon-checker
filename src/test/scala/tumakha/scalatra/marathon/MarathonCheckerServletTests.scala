28ea4c9ee47d4b7bc96927bdd2099c6f8e963288
Yuriy Tumakha <tumakha@gmail.com>
2018-05-20 01:31:35 +0100

    / returns 500

--- src/test/scala/tumakha/scalatra/marathon/MarathonCheckerServletTests.scala
+++ src/test/scala/tumakha/scalatra/marathon/MarathonCheckerServletTests.scala
@@ -6,7 +6,7 @@ class MarathonCheckerServletTests extends ScalatraFunSuite {
 
   addServlet(classOf[MarathonCheckerServlet], "/*")
 
-  test("GET / on MarathonCheckerServlet should return status 200") {
+  test("GET / on MarathonCheckerServlet should return status 500") {
     get("/") {
       status should equal (500)
     }
